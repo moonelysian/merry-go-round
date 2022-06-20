a0f93af838a64a1454f269d489b9559101aea463
moonelysian <dandyblack@naver.com>
2022-06-20 16:47:32 +0900

    add: 스크롤 따라서 동글동글 돌 수 있게

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
 import '../css/style.css';
+
+const logo = document.querySelector('img');
+let lastScrollPosition = 0;
+let currentRadius = 0;
+
+const roundImage = () => {
+  const currentScrollPosition = window.scrollY;
+  const radius =
+    lastScrollPosition > currentScrollPosition
+      ? currentRadius - 0.05
+      : currentRadius + 0.05;
+  logo.style.transform = `rotate(${radius}rad)`;
+  lastScrollPosition = currentScrollPosition;
+  currentRadius = radius;
+};
+
+window.addEventListener('scroll', roundImage);
